d5a85f139ec78036119f1437fb82de0556d8fa39
SuperPox <calebkohl@gmail.com>
2021-01-22 11:14:27 -0700

    commit tree 4258a2f5ad33494014fb6480ccfd3a6861ecbbd3

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount, :lastTransaction, :items
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount 
+        @items = []
+        #@lastTransaction = 0
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total = self.total + (price * quantity)
+        count = 0
+        while count < quantity
+            self.items << title      
+            count += 1
+        end
+
+        #self.lastTransaction = 0  #why is this not resetting!
+        #binding.pry
+        self.lastTransaction = (price * quantity)
+    end
+
+    def apply_discount
+        if self.discount > 0
+            self.total = self.total * ((100 - self.discount) / 100.0)   
+            return "After the discount, the total comes to $#{self.total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.lastTransaction
+        return self.total
+    end
+
+end
 
